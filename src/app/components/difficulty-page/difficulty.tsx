c2192a8323869f86793aeb5b8f3fcf9405b6f700
LeafField <109466326+LeafField@users.noreply.github.com>
2023-07-15 07:32:04 +0900

    Difficulty.tsxの型を追加

--- src/app/components/difficulty-page/difficulty.tsx
+++ src/app/components/difficulty-page/difficulty.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
+import React, { FC } from "react";
 import PageTitle from "./page-title/PageTitle";
 import Aside from "./inner-how-to-use/Aside";
 import ConsoleView from "./console-view/Console-view";
 
-const Difficulty = () => {
+const Difficulty: FC = () => {
   return (
     <div className="grid grid-cols-[25rem_1fr_1fr] gap-x-9  grid-rows-[auto 1fr]  h-[calc(100vh-2.81rem)] px-7">
       <PageTitle title="初級編:Git Hub Flow" />
