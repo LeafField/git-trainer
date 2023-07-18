f25a38db2b6fa3057e2942788482ec3f9cc992f4
LeafField <109466326+LeafField@users.noreply.github.com>
2023-07-19 00:09:29 +0900

    HowToページのレスポンシブの修正

--- src/app/components/how-to-page/HowToPage.tsx
+++ src/app/components/how-to-page/HowToPage.tsx
@@ -5,47 +5,47 @@ import Button from "../button/Button";
 
 const HowToPage = () => {
   return (
-    <main className="md:px-12 lg:px-40 pb-24">
-      <h1 className="text-4xl md:text-6xl xl:text-8xl font-bold text-center text-white pt-[7.5rem]">
+    <main className="px-6 pb-24 md:px-12 lg:px-40">
+      <h1 className="pt-[7.5rem] text-center text-4xl font-bold text-white md:text-6xl xl:text-8xl">
         Git Emptyの遊び方
       </h1>
-      <figure className="relative max-w-4xl mx-auto  container mt-24 md:mt-[11.25rem]">
+      <figure className="container relative mx-auto  mt-24 max-w-4xl md:mt-[11.25rem]">
         <Image
           src={howToImage}
           width={1605}
           height={856}
           style={{ objectFit: "cover", transition: "all 0.25s ease-in-out" }}
           alt="Git Emptyの遊び方参考画像"
-          className="w-full h-auto transition-all"
+          className="h-auto w-full transition-all"
           sizes="100vw"
           priority
           placeholder="blur"
         />
       </figure>
-      <div className="max-w-4xl mx-auto">
-        <div className="pt-[7.5rem] text-base space-y-4 text-white">
+      <div className="mx-auto max-w-4xl">
+        <div className="space-y-4 pt-[7.5rem] text-base text-white">
           <p>
             コンソール上部にGitの練習課題が表示されるので該当するgitコマンドを入力してEnterを押してください。
           </p>
           <p>分からない時は右上の検索窓から検索してみてください。</p>
         </div>
 
-        <div className="text-white mt-[5.62rem]">
+        <div className="mt-[5.62rem] text-white">
           <h2 className="text-3xl font-bold">
             フォルダ名、リモート名などの注意
           </h2>
-          <p className="text-base mt-[2.25rem]">
+          <p className="mt-[2.25rem] text-base">
             リモートリポジトリのURLは「URL」と打ってください
             <br />
             例:git remote add origin URL
           </p>
-          <p className="text-base mt-4">
+          <p className="mt-4 text-base">
             フォルダ名を指定する場合は「.」と打ってください
             <br />
             例:git add .
           </p>
         </div>
-        <div className="w-fit mx-auto mt-[5.62rem]">
+        <div className="mx-auto mt-[5.62rem] w-fit">
           <Button title="Let's Play!" link="/difficulty" color="primary" />
         </div>
       </div>
