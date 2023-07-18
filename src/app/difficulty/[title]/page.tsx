e876d306e6d2889818e4afcce73d721664a073c0
LeafField <109466326+LeafField@users.noreply.github.com>
2023-07-19 01:59:55 +0900

    新しいフェッチパターンのテスト

--- src/app/difficulty/[title]/page.tsx
+++ src/app/difficulty/[title]/page.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Metadata } from "next";
-import { fetcher, fetchTitle } from "../../../libs/fetcher";
+import { fetcher, fetchTitle, preload } from "../../../libs/fetcher";
 import MainPage from "../../components/difficulty-page/Main";
+import MainFetcher from "../../components/difficulty-page/MainFetcher";
 
-export const generateMetadata = async ({
-  params,
-}: Props): Promise<Metadata> => {
-  const title = await fetchTitle(params.title);
-  return {
-    title: `${title} | Git Empty`,
-    description:
-      "Gitコマンドを手軽に空打ちできるサイトです。Git学習後の練習にどうぞ",
-  };
-};
+// export const generateMetadata = async ({
+//   params,
+// }: Props): Promise<Metadata> => {
+//   const title = await fetchTitle(params.title);
+//   return {
+//     title: `${title} | Git Empty`,
+//     description:
+//       "Gitコマンドを手軽に空打ちできるサイトです。Git学習後の練習にどうぞ",
+//   };
+// };
 
 type Props = {
   params: {
@@ -21,9 +22,11 @@ type Props = {
 };
 
 const ConsolePage = async ({ params }: Props) => {
-  const data = await fetcher(params.title);
-  const title = await fetchTitle(params.title);
-  return <MainPage data={data} title={title} />;
+  // const data = await fetcher(params.title);
+  // const title = await fetchTitle(params.title);
+  // return <MainPage data={data} title={title} />;
+  preload(params.title);
+  return <MainFetcher slug={params.title} />;
 };
 
 export default ConsolePage;
