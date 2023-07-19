52a2abea15c49d5e036a15e34bbb4de95443f9f0
LeafField <109466326+LeafField@users.noreply.github.com>
2023-07-19 22:36:34 +0900

    OGP情報の追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,14 @@ export const metadata: Metadata = {
   title: "Git Empty",
   description:
     "Gitコマンドを手軽に空打ちできるサイトです。Git学習後の練習にどうぞ",
+  openGraph: {
+    title: "Git Empty",
+    description:
+      "Gitコマンドを手軽に空打ちできるサイトです。Git学習後の練習にどうぞ",
+    type: "website",
+    url: "https://git-empty.vercel.app/",
+    images: "https://git-empty.vercel.app/public/howto.jpg",
+  },
 };
 
 const Home: NextPage = () => {
